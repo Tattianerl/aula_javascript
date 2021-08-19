042094e8b94961068a957d3b5aac3d08518fac60
tattianerl <tattiane85@hotmail.com>
2021-08-18 22:42:54 -0300

    Introdução ao Javascript

---
+++ Js/main.js
@@ -0,0 +1,151 @@
+
+/*
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b>";
+   // console.log(document.getElementById("agradecimento"));
+    //alert("Obrigada por clicar");
+}
+
+
+function redirecionar(){
+    window.open("https://globallabs.academy/");
+   //window.location.href = "https://global.labs.academy/";
+}
+
+function trocar(elemento){
+   // document.getElementById("mousemove").innerHTML = "<b>Obrigado por passar o mouse</b>"; 
+    elemento.innerHTML = "Obrigado por passar o mouse";
+//alert("trocar texto");
+}
+
+function voltar(elemento){
+  document.getElementById("mousemove").innerHTML = "Passe o mouse aqui";
+  elemento.innerHTML ="Passe o mouse aqui";
+}
+
+function load(){
+    alert("pagina carregada");
+}
+
+function funcaochange(elemento){
+    console.log(elemento.value)
+}
+
+*/
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+
+alert(soma(5, 10));
+*/
+
+/*
+function validaidade(idade){
+    var validar;
+    if (idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+       return validar;
+}
+
+var idade = prompt("Qual sua idade");
+console.log(validaidade(idade));
+*/
+
+
+/*
+function setreplace(frase, nome, novo_nome){
+    return frase.setreplace(nome, novo_nome)
+}
+
+alert(setreplece("Vai Japão", "Japão", "Brasil"));
+*/
+
+
+/*
+var d = new date();
+alert(d);
+alert(d.getMonth());
+alert(d.getminutes());
+alert(d.getday());
+*/
+
+/*
+var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1;
+    count++;
+    alert (count);
+};
+*/
+/*
+var idade = prompt("Qual sua idade");
+if  (idade >= 18){
+        alert("maior de idade");
+
+    }else{
+        alert("menor de idade");
+    };
+    */
+
+
+
+
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor"roxa"}]
+console.log(frutas.nome);
+alert(frutas[1].cor);
+*/
+
+
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+var lista = ["maça", "pêra", "laranja"];
+//lista.pop()
+//lista.push("uva");
+//console.log(lista.length);
+//console.log(lista[1]);
+//alert(lista[1]);
+console.log(lista.reverse());
+console.log(lista);
+console.log(lista.toString);
+console.log(lista.join(" - "));
+*/
+
+
+
+
+
+
+//var nome = "Rafael galleani";
+//var n1 = 5;
+//var n2 = 3;
+//var frase = "Japão é o melhor time do mundo";
+//alert (nome + " tem " + idade + "anos");
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.tolowercase());
+
+//alert(frase.replace("Japão", "Brasil"));
